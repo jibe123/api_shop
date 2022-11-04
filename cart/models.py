37d1c220e96c31ba01252bcc5ad185cf670223ca
jibe123 <jibek.k@gmail.com>
2022-11-05 02:17:53 +0600

    updated files

--- cart/models.py
+++ cart/models.py
@@ -10,14 +10,3 @@ class Cart(models.Model):
     created_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     total = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     order_items = models.ManyToManyField(Product)
-
-    @property
-    def get_total(self):
-        ordered_items = self.order_items.all()
-        total = sum(item.price for item in ordered_items)
-        return total
-
-    def save(self, *args, **kwargs):
-        self.total = self.get_total()
-        super().save(*args, **kwargs)
-
--- cart/serializers.py
+++ cart/serializers.py
@@ -5,7 +5,15 @@ from cart.models import Cart
 
 class CartSerializer(serializers.ModelSerializer):
     created_by = serializers.CurrentUserDefault()
+    total = serializers.SerializerMethodField()
 
     class Meta:
         model = Cart
         fields = '__all__'
+
+    @staticmethod
+    def get_total(obj):
+        return sum(item.price for item in obj.order_items.all())
+
+
+
